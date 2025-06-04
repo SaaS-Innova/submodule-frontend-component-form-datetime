725cef4e0adafc0635698ec22d7af21bbccfee3a
Sakshi Antala <sakshi@saasinnova.com>
2025-06-04 17:32:46 +0530

    Add date increase and decrease button for date time (requested by medqdoc)

--- DateTime.tsx
+++ DateTime.tsx
@@ -8,7 +8,7 @@ import { useTranslation } from "react-i18next";
 import { useMemo } from "react";
 
 const DateTime = (props: IFormProps) => {
-  const { attribute, form, appendTo, fieldType } = props;
+  const { attribute, form, appendTo, fieldType, showAdjustButtons } = props;
   const { label, minDate, maxDate, placeholder } = form[attribute];
   const {
     required,
@@ -57,6 +57,10 @@ const DateTime = (props: IFormProps) => {
       </span>
     </label>
   );
+  const types = [
+    { label: "D", type: "day" as const },
+    { label: "V", type: "week" as const },
+  ];
   return (
     <div className={fieldClassName}>
       {fieldType !== IFormFieldType.NO_LABEL && labelElement}
@@ -66,28 +70,74 @@ const DateTime = (props: IFormProps) => {
           control={control}
           rules={inputValidator(form[attribute].rules, label)}
           render={({ field }) => {
+            const adjustDate = (type: string, amount: number) => {
+              const newDate = timestampToDate(field.value);
+              if (!newDate) return;
+              newDate.setDate(newDate.getDate() + amount);
+              field.onChange(newDate.getTime().toString());
+            };
             return (
-              <Calendar
-                className={`w-full ${errors[attribute] ? "p-invalid" : ""}`}
-                showTime={showTime}
-                hourFormat="12"
-                id={field.name}
-                value={timestampToDate(field.value)}
-                onChange={(e) => {
-                  const timeStamp = e.value
-                    ? new Date(Number(e.value)).getTime().toString()
-                    : null;
-                  field.onChange(timeStamp);
-                }}
-                dateFormat={format}
-                showIcon={showIcon}
-                placeholder={placeholder || defaultPlaceHolder}
-                minDate={minDateValue}
-                maxDate={maxDateValue}
-                appendTo={appendTo}
-                disabled={disabled}
-                view={view}
-              />
+              <div className="p-inputgroup relative">
+                <Calendar
+                  className={`w-full ${errors[attribute] ? "p-invalid" : ""}`}
+                  showTime={showTime}
+                  hourFormat="12"
+                  id={field.name}
+                  value={timestampToDate(field.value)}
+                  onChange={(e) => {
+                    const timeStamp = e.value
+                      ? new Date(Number(e.value)).getTime().toString()
+                      : null;
+                    field.onChange(timeStamp);
+                  }}
+                  dateFormat={format}
+                  showIcon={showIcon}
+                  placeholder={placeholder || defaultPlaceHolder}
+                  minDate={minDateValue}
+                  maxDate={maxDateValue}
+                  appendTo={appendTo}
+                  disabled={disabled}
+                  view={view}
+                />
+                {showAdjustButtons &&
+                  types.map(({ label, type }) => (
+                    <span
+                      key={type}
+                      className="absolute align-items-center flex justify-content-center border-round-2xl bg-red-400 border-transparent w-4rem h-full "
+                      style={{
+                        right: `${type == "day" ? "6.5rem" : "2.3rem"}`,
+                        zIndex: 1,
+                      }}
+                    >
+                      <i
+                        className="pi pi-minus text-white cursor-pointer"
+                        onClick={() => {
+                          if (type === "week") {
+                            adjustDate(type, -7);
+                          } else {
+                            adjustDate(type, -1);
+                          }
+                        }}
+                        style={{ fontSize: "8px" }}
+                      />
+                      <span className="text-white" style={{ fontSize: "12px" }}>
+                        {" "}
+                        {label}{" "}
+                      </span>
+                      <i
+                        className="pi pi-plus text-white cursor-pointer"
+                        onClick={() => {
+                          if (type === "week") {
+                            adjustDate(type, 7);
+                          } else {
+                            adjustDate(type, 1);
+                          }
+                        }}
+                        style={{ fontSize: "8px" }}
+                      />
+                    </span>
+                  ))}
+              </div>
             );
           }}
         />
